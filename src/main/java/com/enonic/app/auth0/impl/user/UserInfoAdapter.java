18565a70c3d4e45cbb0da8b3d4deed77e8c6ea4a
Glenn Ricaud <gri@enonic.com>
2018-03-06 15:09:27 +0100

    Lock & Mvc-auth-commons migration (#13)

--- src/main/java/com/enonic/app/auth0/impl/user/UserInfoAdapter.java
+++ src/main/java/com/enonic/app/auth0/impl/user/UserInfoAdapter.java
@@ -1,5 +1,6 @@
 package com.enonic.app.auth0.impl.user;
 
+import java.time.Instant;
 import java.util.Map;
 
 import com.auth0.json.auth.UserInfo;
@@ -22,15 +23,27 @@ public class UserInfoAdapter
     }
 
     @Override
-    public String getEmail()
-    {
-        return (String) values.get( "email" );
-    }
-
-    @Override
     public String getName()
     {
         return (String) values.get( "name" );
+    }
+
+    @Override
+    public String getFamilyName()
+    {
+        return (String) values.get( "family_name" );
+    }
+
+    @Override
+    public String getGivenName()
+    {
+        return (String) values.get( "given_name" );
+    }
+
+    @Override
+    public String getMiddleName()
+    {
+        return (String) values.get( "middle_name" );
     }
 
     @Override
@@ -43,6 +56,30 @@ public class UserInfoAdapter
     public String getPicture()
     {
         return (String) values.get( "picture" );
+    }
+
+    @Override
+    public Instant getUpdatedAt()
+    {
+        final String updatedAt = (String) values.get( "updated_at" );
+        if ( updatedAt != null )
+        {
+            try
+            {
+                return Instant.parse( updatedAt );
+            }
+            catch ( Exception e )
+            {
+
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getEmail()
+    {
+        return (String) values.get( "email" );
     }
 
     @Override
