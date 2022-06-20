639c4861ab2d3a8c2d5bb4a4f7bd39d43c1069ed
rymsha <rymsha@yandex.ru>
2022-06-20 23:07:32 +0200

    Don't use Apache HttpClient query parser #65

--- src/main/java/com/enonic/app/auth0/impl/utils/QueryParamUtils.java
+++ src/main/java/com/enonic/app/auth0/impl/utils/QueryParamUtils.java
@@ -1,71 +1,69 @@
 package com.enonic.app.auth0.impl.utils;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
+import java.util.stream.Collectors;
 
 public class QueryParamUtils
 {
-    public static String addOrReplaceInQueryParams(final String queryParams, final String key, final String value) {
-        Objects.requireNonNull(queryParams);
-        Objects.requireNonNull(key);
-        Objects.requireNonNull(value);
-        final StringBuilder builder = new StringBuilder();
-        final String updatedQueryParams = removeFromQueryParams(queryParams, key);
-        if (updatedQueryParams.isEmpty()) {
-            builder.append(key).append("=").append(value);
-        } else {
-            builder.append(updatedQueryParams).append("&").append(key).append("=").append(value);
-        }
-        return builder.toString();
+    public static String addOrReplaceInQueryParams( final String queryParams, final String key, final String value )
+    {
+        Objects.requireNonNull( queryParams );
+        Objects.requireNonNull( key );
+        Objects.requireNonNull( value );
+
+        final Map<String, String> map = parse( queryParams );
+        map.put( key, value );
+
+        return format( map );
     }
 
-    public static String parseFromQueryParams(final String queryParams, final String key) {
-        Objects.requireNonNull(queryParams);
-        Objects.requireNonNull(key);
-        final List<NameValuePair> params = URLEncodedUtils.parse(queryParams, StandardCharsets.UTF_8);
-        for (final NameValuePair param : params) {
-            if (key.equals(param.getName())) {
-                return param.getValue();
-            }
-        }
-        return null;
+    public static String parseFromQueryParams( final String queryParams, final String key )
+    {
+        Objects.requireNonNull( queryParams );
+        Objects.requireNonNull( key );
+        return parse( queryParams ).get( key );
     }
 
-    public static boolean keyInQueryParams(final String queryParams, final String key) {
-        Objects.requireNonNull(queryParams);
-        Objects.requireNonNull(key);
-        final List<NameValuePair> params = URLEncodedUtils.parse(queryParams, StandardCharsets.UTF_8);
-        for (final NameValuePair param : params) {
-            if (key.equals(param.getName())) {
-                return true;
+    private static Map<String, String> parse( final String queryParams )
+    {
+        final Map<String, String> params = new LinkedHashMap<>();
+        final String[] pairs = queryParams.split( "&", -1 );
+        for ( final String pair : pairs )
+        {
+            final String[] split = pair.split( "=", 2 );
+            if ( split.length == 2 )
+            {
+                params.put( urlDecode( split[0] ), urlDecode( split[1] ) );
             }
         }
-        return false;
+        return params;
     }
 
-    public static String removeFromQueryParams(final String queryParams, final String key) {
-        Objects.requireNonNull(queryParams);
-        Objects.requireNonNull(key);
-        final List<NameValuePair> params = URLEncodedUtils.parse( queryParams, StandardCharsets.UTF_8);
-        final List<NameValuePair> newParams = new ArrayList<>();
-        for (final NameValuePair param : params) {
-            if (!key.equals(param.getName())) {
-                newParams.add(param);
-            }
+    private static String format( final Map<String, String> params )
+    {
+        if ( params.isEmpty() )
+        {
+            return "";
         }
-        final String newQueryStringEncoded = URLEncodedUtils.format(newParams, StandardCharsets.UTF_8);
-        try {
-            final String newQueryStringDecoded = URLDecoder.decode( newQueryStringEncoded, StandardCharsets.UTF_8.toString());
-            return newQueryStringDecoded;
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalStateException("Failed to decode query param " + e.getLocalizedMessage());
-        }
+
+        return params.entrySet()
+            .stream()
+            .map( entry -> urlEncode( entry.getKey() ) + "=" + urlEncode( entry.getValue() ) )
+            .collect( Collectors.joining( "&" ) );
+    }
+
+    private static String urlEncode( final String value )
+    {
+        return URLEncoder.encode( value, StandardCharsets.UTF_8 );
+    }
+
+    private static String urlDecode( final String value )
+    {
+        return URLDecoder.decode( value, StandardCharsets.UTF_8 );
     }
 }
